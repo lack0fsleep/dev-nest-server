e0494d7a706b66770dabafae919487ae7870bd1c
vv.kulesh <vv.kulesh@tensor.ru>
2021-08-12 16:27:49 +0300

    mongo connect

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,21 +1,34 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { ReviewModel } from './review.model';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { REVIEW_NOT_FOUND } from './review.constants';
+import { ReviewService } from './review.service';
 
 @Controller('review')
 export class ReviewController {
 
+    constructor(private readonly reviewService: ReviewService) {}
+
     @Post('create')
-    async create(@Body() dto: Omit<ReviewModel, '_id'>) {
-        return;
+    async create(@Body() dto: CreateReviewDto) {
+        return this.reviewService.create(dto);
     }
 
     @Delete(':id')
     async delete(@Param('id') id: string) {
-        return;
+        const deletedDoc = await this.reviewService.delete(id);
+        console.log('ttttt', deletedDoc);
+        if (!deletedDoc) {
+            throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+        }
     }
 
     @Get('byProduct/:productId')
     async getByProduct(@Param('productId') productId: string) {
-        return;
+        return this.reviewService.findByProductId(productId);
+    }
+
+    @Delete('byProduct/:productId')
+    async deleteByProduct(@Param('productId') productId: string) {
+        return this.reviewService.deleteByProductId(productId);
     }
 }
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,12 +1,9 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model';
 
 @Controller('top-page')
 export class TopPageController {
-
-    constructor(private readonly configService: ConfigService) {}
 
     @Post('create')
     async create(@Body() dto: Omit<TopPageModel, '_id'>) {
@@ -15,7 +12,7 @@ export class TopPageController {
 
     @Get(':id')
     async get(@Param('id') id: string) {
-        return this.configService.get('TEST');
+        return;
     }
 
     @Delete(':id')
